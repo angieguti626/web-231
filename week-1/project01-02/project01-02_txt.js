56fa4dc8fea663e2f835d00b39dc42ed1ba74e5f
Angelica Gutierrez <angelicagu368@gmail.com>
2024-03-24 17:11:27 -0700

    js file updated

--- week-1/project01-02/project01-02_txt.js
+++ week-1/project01-02/project01-02_txt.js
@@ -2,10 +2,18 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Angelica Gutierrez
+    Date:   3/24/24
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+    var service1Name = "Basic";
+    var service2Name = "Express";
+    var service3Name = "Extreme";
+    var service4Name = "Ultimate";
+    var service1Speed = "50 Mbsp";
+    var service2Speed = "100 Mbsp";
+    var service3Speed = "500 Mbsp";
+    var service4Speed = "1 Gig";
